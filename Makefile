c16948d3f663fdf4796bbe5aa7a7b5d084ba1b3a
K Lauer <klauer@slac.stanford.edu>
2020-05-27 16:27:39 -0700

    REF: fixes + cleanup

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+all: adcore_convert
+
 adcore_clone:
 	@if [[ ! -d adcore ]]; then \
 		git clone https://github.com/areaDetector/adcore && \
@@ -5,8 +7,10 @@ adcore_clone:
 	fi
 	@echo "ADCore available."
 
-adcore_convert:
+adcore_convert: adcore_clone
+	dest_path=$${PWD}/displays; \
 	convert_path=$${PWD}/conversion; \
+	mkdir -p $${dest_path} ; \
 	pushd adcore; \
 		for release in $$(git tag |grep -v -e beta); do \
 			echo "\n**** $${release} ****"; \
@@ -14,7 +18,13 @@ adcore_convert:
 			rm -f $${convert_path}/*.ui; \
 			adl2pydm -d $${convert_path} $$(find . -name "*.adl"); \
 			pushd $${convert_path} && \
-				python rename_converted_adcore.py $${release}; \
+				python rename_converted_adcore.py $${release} "$${dest_path}"; \
 			popd; \
+			rm -f $${convert_path}/*.ui; \
 		done; \
 	popd
+
+clean:
+	rm -f displays/*.ui
+
+.PHONY: adcore_clone adcore_convert clean
--- conversion/rename_converted_adcore.py
+++ conversion/rename_converted_adcore.py
@@ -2,16 +2,8 @@ import os
 import re
 import sys
 
-adcore_release_tag = sys.argv[1]
-
-# R3-2 -> V32
-class_suffix = adcore_release_tag.replace('_', '').replace('-', '')
-class_suffix = 'V' + class_suffix[1:]
-
-source_extension = '.ui'
-output_extension = f'_{class_suffix}.ui'  # .detailed.ui?
-
-filename_map = {
+SOURCE_EXTENSION = '.ui'
+FILENAME_MAP = {
     "ADAttrFile": "ADAttrFile",
     "ADBase": "_ADBase",  # do not match with ADBase class
     "ADBuffers": "ADBuffers",
@@ -80,32 +72,27 @@ filename_map = {
 
     #
     "NDTimeSeriesAll": "TimeSeriesAll",
-    "simDetector": "SimDetector",
+    "simDetector": "SimDetectorCam",
     "simDetectorSetup": "SimDetectorSetup",
     "ADPvaDriver": "ADPvaDriver",
 }
 
 
-# Tag on the extensions
-filename_map = {
-    source + source_extension: dest + output_extension
-    for source, dest in filename_map.items()
-}
+def get_filename_map(output_extension):
+    return {
+        source + SOURCE_EXTENSION: dest + output_extension
+        for source, dest in FILENAME_MAP.items()
+    }
 
-replace_regex = {
-    re.compile(r'\b' + source + r'\b'): dest
-    for source, dest in filename_map.items()
-}
 
-additional = list(display for display in
-                  sorted(set(os.listdir()) - set(filename_map))
-                  if os.path.splitext(display)[1] == '.ui')
+def get_class_suffix(adcore_release_tag):
+    """R3-2 -> V32"""
+    class_suffix = adcore_release_tag.replace('_', '').replace('-', '')
+    return 'V' + class_suffix[1:]
 
-for source, dest in filename_map.items():
-    print('*', source, '->', dest)
-    if not os.path.exists(source):
-        continue
 
+def fix_ui_source(source, dest, replace_regex):
+    """Fix the .ui source, as related display names have been renamed."""
     with open(source, 'rt') as f:
         contents = f.read()
 
@@ -118,7 +105,68 @@ for source, dest in filename_map.items():
         print(contents, file=f)
 
 
-if additional:
-    for screen in additional:
-        print('!! Removing screen that was not renamed:', screen)
-        os.unlink(screen)
+def convert(adcore_release_tag, dest_path, *, class_suffix=None,
+            output_extension=None):
+    """
+    Given the release tag, rename screens to typhos-compatible ones.
+
+    Parameters
+    ----------
+    adcore_release_tag : str
+        The release tag.
+
+    dest_path : str
+        The destination directory for the screens.
+
+    class_suffix : str, optional
+        Automatically determined by the release tag. e.g., ``R3-2`` is turned
+        into a class suffix of ``_V32``.
+
+    output_extension : str, optional
+        Automatically determined by the release tag or class suffix. Defaults
+        to f'_{class_suffix}.ui'.
+    """
+
+    if class_suffix is None:
+        class_suffix = get_class_suffix(adcore_release_tag)
+
+    if output_extension is None:
+        output_extension = f'_{class_suffix}.ui'  # .detailed.ui?
+
+    filename_map = get_filename_map(output_extension)
+    replace_regex = {
+        re.compile(r'\b' + source + r'\b'): dest
+        for source, dest in filename_map.items()
+    }
+
+    replace_regex[re.compile(r'\${P}\${R}')] = '${prefix}'
+
+    handled_filenames = set(list(filename_map) + list(filename_map.values()))
+    other_displays = list(
+        display for display in os.listdir()
+        if os.path.splitext(display)[1] == '.ui' and
+        display not in handled_filenames
+    )
+
+    for source, dest in filename_map.items():
+        if os.path.exists(source):
+            print('*', source, '->', dest)
+            fix_ui_source(source, os.path.join(dest_path, dest), replace_regex)
+
+    for display in other_displays:
+        print('!! Unhandled display file:', display)
+
+
+if __name__ == '__main__':
+    adcore_release_tag = sys.argv[1]
+    assert adcore_release_tag.startswith('R'), 'Bad release tag?'
+
+    dest_path = sys.argv[2]
+    assert os.path.exists(dest_path) and os.path.isdir(dest_path), 'Dest path?'
+
+    if adcore_release_tag == 'R2-0':
+        print('* Special-casing R2-0 to generate versionless screens.')
+        convert(adcore_release_tag, dest_path, class_suffix='',
+                output_extension='.ui')
+
+    convert(adcore_release_tag, dest_path)
